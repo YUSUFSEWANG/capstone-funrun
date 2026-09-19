b07ad1dffadbda58b3bdb8b771b8396c12ce36c1
YUSUFSEWANG <YUSUFSEWANG@users.noreply.github.com>
2026-09-19 20:01:24 +0800

    Dukung koneksi MySQL ber-SSL (Aiven)

--- config/database.php
+++ config/database.php
@@ -59,7 +59,10 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                // Nilai relatif diselesaikan dari root project agar jalan di lokal maupun Vercel.
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA')
+                    ? base_path(env('MYSQL_ATTR_SSL_CA'))
+                    : null,
             ]) : [],
         ],
 
